5672e2fcc82a9efdbfa9c3155f9743b63b820cb4
Drew Beckmen <drew.beckmen@yale.edu>
2020-06-08 17:23:08 -0400

    added some reading methods in User to filter out expenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,43 @@
+# Extending the date class (https://stackoverflow.com/questions/42712101/if-date-is-less-than-a-week-ago-conditional)
+class Date 
+    def within_last?(duration, date = Date.current)
+        between?(date - duration, date)
+    end
+end 
+
 class User < ActiveRecord::Base
     has_many :expenses
     has_many :categories, through: :expenses
+
+    # Returns the total expenses logged in this platform by a specific User instance
+    def total_expenses
+        sum_expenses(self.expenses)
+    end
+
+    # Returns array of this user's expenses from today to today - duration days ago
+    def get_specific_expenses(duration)
+        self.expenses.select {|expense| expense.logged_on.within_last?(duration)}
+    end
+
+    # Returns total expenses logged by this user in the previous week
+    def total_week_expenses
+        sum_expenses(self.get_specific_expenses(7))
+    end 
+
+    # Returns total expenses logged by this user in the previous month (30 days)
+    def total_month_expenses
+        sum_expenses(self.get_specific_expenses(30))
+    end 
+
+    # Returns total expenses logged by this user in the previous year
+    def total_year_expenses
+        sum_expenses(self.get_specific_expenses(365))
+    end 
+
+    # Define a private instance method that should only be used internally to sum
+    # instances of Expense for a given user 
+    private 
+    def sum_expenses(list_expenses)
+        list_expenses.inject(0) {|sum, expense| sum + expense.amount}
+    end 
 end 
