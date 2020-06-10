b57394b435a0db18e7d74d692174ad6a9280c004
Drew Beckmen <drew.beckmen@yale.edu>
2020-06-10 10:33:03 -0400

    added a live currency exchange calculator to app

--- bin/run.rb
+++ bin/run.rb
@@ -89,16 +89,52 @@ def main_menu
     end
 end 
 
+
+def get_currency(base_or_target)
+    currency = $prompt.ask("What is the three letter code for your #{base_or_target} currency? Want to select from a list? Press 1: ")
+    case currency
+    when "1"
+        currency = $prompt.select("Choose a currency: ", $SUPPORTED_CURRENCIES)
+    else 
+        while !valid_currency?(currency)
+            puts "Sorry, #{currency} is not a supported currency."
+            currency = $prompt.ask("Please enter a valid 3 letter code for your #{base_or_target} currency: ")
+        end 
+    end 
+    currency
+end 
+
+def get_amount 
+    amount = $prompt.ask("Please enter an amount of money to convert: ")
+    amount.to_f  #may want to come back to this later to do some error handling if it can't be converted. 
+end 
+
+
+def currency_exchange 
+    puts "Welcome to the built in currency exchange calculator!"
+    puts "We use live forex rates fetched from a trusted API!"
+    base_currency = get_currency("base")
+    amount = get_amount
+    target_currency = get_currency("target")
+    puts "#{amount} #{base_currency} is #{convert_currency(base_currency, target_currency, amount).round(2)} #{target_currency}"
+    puts "Thank you for using the currency exchange calculator"
+end 
+
 # Master method to run whole program 
 def run 
     system "clear"
     active_user = greet_user
-    action = main_menu
-    case action 
-    when "Quit the program."
-        puts "Thank you for using Xpense!"
-        return 0
-    end 
+    while true 
+        action = main_menu
+        case action
+        when "Currency exchange calculator."
+            currency_exchange
+        when "Quit the program."
+            puts "Thank you for using Xpense!"
+            return 0
+        end
 end 
 
-run
+# run
+
+currency_exchange
